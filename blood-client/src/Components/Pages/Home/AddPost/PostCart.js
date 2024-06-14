ebc2148e7b8cfc0228e647566a7a7b425c2242ad
dipankar-halder <dipankar15-11137@diu.edu.bd>
2024-06-14 13:22:07 +0600

    solve error

--- blood-client/src/Components/Pages/Home/AddPost/PostCart.js
+++ blood-client/src/Components/Pages/Home/AddPost/PostCart.js
@@ -1,7 +1,8 @@
+
 import React from 'react';
 import { useForm } from 'react-hook-form';
 
-const PostCart = ({user}) => {
+const PostCart = ({ user }) => {
   const imageHostKey = 'c70a5fc10619997bd7315f2bf28d0f3e';
 
   const currentDate = new Date();
@@ -17,72 +18,67 @@ const PostCart = ({user}) => {
   // Format the current time
   const formattedTime = `${year}-${month}-${date}T${hours}:${minutes}:${seconds}`;
 
-  // 
-   const {
-     register,
-     formState: { errors },
-     handleSubmit,
-     reset,
-   } = useForm();
-  const onSubmit = (data) => {
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+    reset,
+  } = useForm();
+
+  const onSubmit = async data => {
     if (data.img[0]) {
-  const image = data.img[0];
-  const formData = new FormData();
-  formData.append('image', image);
-  const url = `https://api.imgbb.com/1/upload?expiration=600&key=${imageHostKey}`;
-  fetch(url, {
-    method: 'POST',
-    body: formData,
-  })
-    .then(res => res.json())
-    .then(imageData => {
-      const image = imageData.data.url;
+      const image = data.img[0];
+      const formData = new FormData();
+      formData.append('image', image);
+      const url = `https://api.imgbb.com/1/upload?expiration=600&key=${imageHostKey}`;
+      const response = await fetch(url, {
+        method: 'POST',
+        body: formData,
+      });
+      const imageData = await response.json();
+      const imageUrl = imageData.data.url;
+
       const updateUrl = {
         ...data,
         name: user?.name,
         img: user?.img,
         email: user?.email,
-        image,
+        image: imageUrl,
         pTime: formattedTime,
       };
 
-      // console.log('aci',updateUrl);
-      fetch(`http://localhost:5000/quires`, {
+      await fetch(`http://localhost:5000/quires`, {
         method: 'POST',
         headers: {
           'content-type': 'application/json',
         },
         body: JSON.stringify(updateUrl),
-      })
-        .then(res => res.json())
-        .then(data => {
-          reset();
-        });
-    });
+      });
+
+      reset();
+      document.getElementById('my_modal_6').checked = false; // Close the modal
     } else {
-       const updateUrl = {
-         ...data,
-         name: user?.name,
-         img: user?.img,
-         email: user?.email,
-         pTime: formattedTime,
-       };
+      const updateUrl = {
+        ...data,
+        name: user?.name,
+        img: user?.img,
+        email: user?.email,
+        pTime: formattedTime,
+      };
 
-      //  console.log('nai',updateUrl);
-       fetch(`http://localhost:5000/quires`, {
-         method: 'POST',
-         headers: {
-           'content-type': 'application/json',
-         },
-         body: JSON.stringify(updateUrl),
-       })
-         .then(res => res.json())
-         .then(data => {
-           reset();
-         });
-}
+      await fetch(`http://localhost:5000/quires`, {
+        method: 'POST',
+        headers: {
+          'content-type': 'application/json',
+        },
+        body: JSON.stringify(updateUrl),
+      });
 
-  }
+      reset();
+      document.getElementById('my_modal_6').checked = false; // Close the modal
+    }
+  };
+
   return (
     <div className="modal-box">
       <div className="modal-action fixed right-0 -top-6">
@@ -98,21 +94,20 @@ const PostCart = ({user}) => {
           Create Post
         </h1>
       </div>
-
       <div>
         <form onSubmit={handleSubmit(onSubmit)}>
-          <div className="form-control w-full ">
+          <div className="form-control w-full">
             <label className="label">
               <span className="label-text">Post Here</span>
             </label>
             <textarea
               type="text"
               placeholder="Write your post"
-              className="input input-bordered bg-slate-600 w-full  pt-1 h-32 text-slate-100"
+              className="input input-bordered bg-slate-600 w-full pt-1 h-32 text-slate-100"
               {...register('description', {})}
             />
           </div>
-          <div className="form-control w-full ">
+          <div className="form-control w-full">
             <label className="label">
               <span className="label-text">Image</span>
             </label>
@@ -120,14 +115,9 @@ const PostCart = ({user}) => {
               type="file"
               placeholder="Image"
               className="input input-bordered bg-slate-600 w-full pt-1"
-              {...register('img', {
-               
-               
-              })}
+              {...register('img', {})}
             />
-           
           </div>
-
           <input
             className="btn btn-primary w-full text-white mt-3"
             type="submit"
@@ -140,3 +130,200 @@ const PostCart = ({user}) => {
 };
 
 export default PostCart;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React from 'react';
+// import { useForm } from 'react-hook-form';
+
+// const PostCart = ({user}) => {
+//   const imageHostKey = 'c70a5fc10619997bd7315f2bf28d0f3e';
+
+//   const currentDate = new Date();
+
+//   // Get the current year, month, date, hours, minutes, and seconds
+//   const year = currentDate.getFullYear();
+//   const month = String(currentDate.getMonth() + 1).padStart(2, '0'); // Months are zero-based
+//   const date = String(currentDate.getDate()).padStart(2, '0');
+//   const hours = String(currentDate.getHours()).padStart(2, '0');
+//   const minutes = String(currentDate.getMinutes()).padStart(2, '0');
+//   const seconds = String(currentDate.getSeconds()).padStart(2, '0');
+
+//   // Format the current time
+//   const formattedTime = `${year}-${month}-${date}T${hours}:${minutes}:${seconds}`;
+
+//   //
+//    const {
+//      register,
+//      formState: { errors },
+//      handleSubmit,
+//      reset,
+//    } = useForm();
+//   const onSubmit = (data) => {
+//     if (data.img[0]) {
+//   const image = data.img[0];
+//   const formData = new FormData();
+//   formData.append('image', image);
+//   const url = `https://api.imgbb.com/1/upload?expiration=600&key=${imageHostKey}`;
+//   fetch(url, {
+//     method: 'POST',
+//     body: formData,
+//   })
+//     .then(res => res.json())
+//     .then(imageData => {
+//       const image = imageData.data.url;
+//       const updateUrl = {
+//         ...data,
+//         name: user?.name,
+//         img: user?.img,
+//         email: user?.email,
+//         image,
+//         pTime: formattedTime,
+//       };
+
+//       // console.log('aci',updateUrl);
+//       fetch(`http://localhost:5000/quires`, {
+//         method: 'POST',
+//         headers: {
+//           'content-type': 'application/json',
+//         },
+//         body: JSON.stringify(updateUrl),
+//       })
+//         .then(res => res.json())
+//         .then(data => {
+//           reset();
+//         });
+//     });
+//     } else {
+//        const updateUrl = {
+//          ...data,
+//          name: user?.name,
+//          img: user?.img,
+//          email: user?.email,
+//          pTime: formattedTime,
+//        };
+
+//       //  console.log('nai',updateUrl);
+//        fetch(`http://localhost:5000/quires`, {
+//          method: 'POST',
+//          headers: {
+//            'content-type': 'application/json',
+//          },
+//          body: JSON.stringify(updateUrl),
+//        })
+//          .then(res => res.json())
+//          .then(data => {
+//            reset();
+//          });
+// }
+
+//   }
+//   return (
+//     <div className="modal-box">
+//       <div className="modal-action fixed right-0 -top-6">
+//         <label
+//           htmlFor="my_modal_6"
+//           className="bg-slate-700 px-3 py-1 rounded-full cursor-pointer"
+//         >
+//           x
+//         </label>
+//       </div>
+//       <div>
+//         <h1 className="text-center -mt-3 font-semibold border-b-[1px] pb-2 border-slate-600">
+//           Create Post
+//         </h1>
+//       </div>
+
+//       <div>
+//         <form onSubmit={handleSubmit(onSubmit)}>
+//           <div className="form-control w-full ">
+//             <label className="label">
+//               <span className="label-text">Post Here</span>
+//             </label>
+//             <textarea
+//               type="text"
+//               placeholder="Write your post"
+//               className="input input-bordered bg-slate-600 w-full  pt-1 h-32 text-slate-100"
+//               {...register('description', {})}
+//             />
+//           </div>
+//           <div className="form-control w-full ">
+//             <label className="label">
+//               <span className="label-text">Image</span>
+//             </label>
+//             <input
+//               type="file"
+//               placeholder="Image"
+//               className="input input-bordered bg-slate-600 w-full pt-1"
+//               {...register('img', {
+
+//               })}
+//             />
+
+//           </div>
+
+//           <input
+//             className="btn btn-primary w-full text-white mt-3"
+//             type="submit"
+//             value="Post"
+//           />
+//         </form>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default PostCart;
--- blood-client/src/Components/Pages/Home/Home Section/Member/Members.js
+++ blood-client/src/Components/Pages/Home/Home Section/Member/Members.js
@@ -4,13 +4,21 @@ import Member from './Member';
 const Members = ({ members }) => {
   return (
     <div>
-      <div className="grid grid-cols-3 mx-3 pt-3 gap-5">
-        {members.slice().reverse().map(member => (
-          <Member key={member._id} member={member} />
-        ))}
-
-      
-      </div>
+      {members.length === 0 ? (
+        <div className='text-center pt-32 text-4xl text-slate-400'>
+          <h1>Not Available At This Time</h1>
+        </div>
+      ) : (
+        <div className="grid grid-cols-3 mx-3 pt-3 gap-5">
+          {members
+            .slice()
+            .reverse()
+            .map(member => (
+              <Member key={member._id} member={member} />
+            ))}
+        </div>
+      )}
+   
     </div>
   );
 };
